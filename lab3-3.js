e411f5d2a0eb7f609d91897e3d550c9da1f17b42
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2015-11-14 19:56:09 -0500

    1) removed 'dedRow' class, probably not the cleanest way to accomplish 2) adjusted JS to reflect (not finished) 3)added basic style to help visiaully represent rows (further adjust to actually style)

--- lab3-3.js
+++ lab3-3.js
@@ -1,6 +1,12 @@
 //button//
 var delButton = document.getElementsByClassName ("btn btn-default");
 
+function delRow() {
+	document.getElementsByClassName ("btn btn-default")[0,1,2];
+}
+
+delButton.addEventListener("click", )
+
 function delThem = 
 
 delButton[0,1,2]
